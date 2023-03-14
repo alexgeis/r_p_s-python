0ff2b06a26290773b1ae680af44ec0abb1ad423e
Alex Geis <siegxela@gmail.com>
2023-03-14 11:19:39 -0600

    add modules for modular approach: game.py, rock_paper_scissors.py, and output.py

--- rps/practice.py
+++ rps/practice.py
@@ -14,4 +14,19 @@ def random_arithmetic(int1: int, int2: int) -> str:
     evalStr = str(int1) + " " + opList[randomIndex] + " " + str(int2)
     evalValue = eval(evalStr)
     return print(evalStr + " = " + str(evalValue))
-random_arithmetic(5, 10)
+# random_arithmetic(5, 10)
+
+def is_vowel(letter:str) -> str:
+    unicodeVal = ord(letter.lower())
+    if (unicodeVal == 97 or
+        unicodeVal == 101 or
+        unicodeVal == 105 or
+        unicodeVal == 111 or
+        unicodeVal == 117):
+        return print(True)
+    else: 
+        return print(False)
+is_vowel("a")
+is_vowel("f")
+is_vowel("o")
+is_vowel("y")
